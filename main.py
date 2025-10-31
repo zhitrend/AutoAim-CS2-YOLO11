a2efbe9f49f6bd6420b5bb36feabd465264034e1
zhitrend <zhangrongwuios@icloud.com>
2025-11-01 00:04:11 +0800

    1

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import time
 
 
 def load_config(path="config.yaml"):
-    with open(path, "r") as f:
+    with open(path, "r", encoding='utf-8') as f:
         return yaml.safe_load(f)
 
 def main():
